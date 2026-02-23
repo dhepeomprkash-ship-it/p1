2a5c943af522597a4865b0e70c187572acf547d1
dhepeomprkash-ship-it <dhepeomprkash@gmail.com>
2026-02-23 21:27:10 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+
+st.title("उसावरील रोग ओळखणे (Sugarcane Disease AI)")
+
+# मॉडेल लोड करणे
+model = tf.keras.models.load_model('sugarcane_model.h5')
+
+# फोटो अपलोड करण्याचे बटण
+uploaded_file = st.file_uploader("उसाच्या पानाचा फोटो निवडा...", type="jpg")
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='अपलोड केलेला फोटो', use_column_width=True)
+    st.write("ओळखत आहे...")
+    
+    # इमेज प्रोसेसिंग
+    img = image.resize((224, 224))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    
+    # निकाल
+    prediction = model.predict(img_array)
+    if np.argmax(prediction) == 0:
+        st.success("निकाल: ऊस निरोगी (Healthy) आहे!")
+    else:
+        st.error("निकाल: उसावर रोग (Diseased) आढळला आहे!")
